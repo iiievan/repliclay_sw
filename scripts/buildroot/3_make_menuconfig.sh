8aaaf2b7bbaa9a615f5389dd6834de0295750f15
Mbo <mbo.kastnev@gmail.com>
2022-12-22 17:06:43 +0300

    making BR2_EXTERNAL directory

--- scripts/buildroot/3_make_menuconfig.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${0}" )" &> /dev/null && pwd )
-cd ..
-./build_env.sh
-
-cd ${BUILDROOT_DIR}
-
-make menuconfig
--- scripts/mscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BE_DIR=$(pwd)
-ROOT_DIR= dirname $BE_DIR
-
-echo $BE_DIR
-echo $ROOT_DIR
