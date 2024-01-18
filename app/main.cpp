69b7298856f7949a9fb849033a4eedb7fa5d1c76
KFT\i.shopov <i.shopov@comitas.ru>
2024-01-19 01:52:57 +0300

    ring buffer upgrade

--- app/main.cpp
+++ app/main.cpp
@@ -4,12 +4,116 @@
 #include "sys_timer.h"
 #include "pin.h"
 #include "am3358zcz_pins.hpp"
+#include "ring_buffer.hpp"
+
+
+typedef union 
+{ 
+    struct 
+    {                                              
+        uint32_t    W_PEND_TCLR :4;           
+        uint32_t    W_PEND_TCRR :4;           
+        uint32_t    W_PEND_TLDR :4;           
+        uint32_t    W_PEND_TTGR :4;                      
+        uint32_t                :16;          
+    } b;                                      
+    uint32_t  z;                            
+} reg_t;
+
+typedef struct
+{
+     reg_t  register_a;
+     reg_t  register_b;
+     reg_t  register_c;
+     reg_t  register_m;
+      char  name[8];
+} test_struct_t;
 
 int main()
 {  
     static uint64_t time = 0;
     
-    init_board();    
+    test_struct_t pop_result;
+    
+    ring_buffer<test_struct_t,8> ringbuffer;
+    
+    test_struct_t fsm_1 = { .register_a = {.z = 0x3245},
+                            .register_b = {.z = 0x3245},
+                            .register_c = {.z = 0x3245},
+                            .register_m = {.z = 0x3245},
+                            .name = "fsm_1"
+                          };
+    test_struct_t fsm_2 = { .register_a = {.z = 0x1234},
+                            .register_b = {.z = 0x1234},
+                            .register_c = {.z = 0x1234},
+                            .register_m = {.z = 0x1234},
+                            .name = "fsm_2"
+                          };
+    test_struct_t fsm_3 = { .register_a = {.z = 0x8989},
+                            .register_b = {.z = 0x8989},
+                            .register_c = {.z = 0x8989},
+                            .register_m = {.z = 0x8989},
+                            .name = "fsm_3"
+                          };
+    test_struct_t fsm_4 = { .register_a = {.z = 0x6545},
+                            .register_b = {.z = 0x6545},
+                            .register_c = {.z = 0x6545},
+                            .register_m = {.z = 0x6545},
+                            .name = "fsm_4"
+                          };
+   test_struct_t fsm_5 = { .register_a = {.z = 0x1010},
+                           .register_b = {.z = 0x1010},
+                           .register_c = {.z = 0x1010},
+                           .register_m = {.z = 0x1010},
+                           .name = "fsm_5"
+                          };
+   
+   test_struct_t fsm_6 = { .register_a = {.z = 0x2828},
+                           .register_b = {.z = 0x2828},
+                           .register_c = {.z = 0x2828},
+                           .register_m = {.z = 0x2828},
+                           .name = "fsm_6"
+                          };
+   
+   test_struct_t fsm_7 = { .register_a = {.z = 0x9876},
+                           .register_b = {.z = 0x9876},
+                           .register_c = {.z = 0x9876},
+                           .register_m = {.z = 0x9876},
+                           .name = "fsm_7"
+                          };
+   
+   test_struct_t fsm_8 = { .register_a = {.z = 0x5467},
+                           .register_b = {.z = 0x5467},
+                           .register_c = {.z = 0x5467},
+                           .register_m = {.z = 0x5467},
+                           .name = "fsm_8"
+                          };
+    
+    init_board(); 
+    
+    ringbuffer.push(fsm_1);
+    ringbuffer.push(fsm_2);
+    ringbuffer.push(fsm_3);
+    ringbuffer.push(fsm_4);
+    ringbuffer.push(fsm_5);
+    ringbuffer.push(fsm_6);
+    ringbuffer.push(fsm_7);
+    ringbuffer.push(fsm_8);
+    
+    pop_result = ringbuffer.pop_item(2);
+                 ringbuffer.push(fsm_4);
+    pop_result = ringbuffer.pop_item(1);
+    pop_result = ringbuffer.pop_item(0);
+    pop_result = ringbuffer.pop_item(1);
+                 ringbuffer.push(fsm_1);
+                 ringbuffer.push(fsm_2);
+                 ringbuffer.push(fsm_3);
+                 ringbuffer.push(fsm_1);
+    pop_result = ringbuffer.pop_item(2);
+    
+    ringbuffer.reset_to_zero();
+    
+    
 
     while(1)
     {      
---
+++ utils/ring_buffer.hpp
@@ -0,0 +1,174 @@
+#ifndef _RINGBUFFER_H
+#define _RINGBUFFER_H
+
+#include "stddef.h"
+#include "stdint.h"
+#include <string.h>         // for memcpy
+
+
+template <typename T, size_t BUF_SIZE = 16>
+class ring_buffer
+{
+public:
+    ring_buffer() : m_Size(BUF_SIZE)
+    {
+        m_Head = m_Tail = m_Avail = 0;
+        memset(m_BUF, 0, sizeof(m_BUF));
+    }
+
+    inline size_t get_avail() const { return m_Avail; }            // Return available data in ringbuffer
+    inline size_t get_free() const { return (m_Size - m_Avail); }  // Return available space in ringbuffer
+    inline size_t get_size() const { return m_Size; }              // Return size of ringbuffer
+    inline bool   is_empty() const { return m_Tail == m_Head; }    // Test if the ringbuffer is empty.
+    inline bool   is_full() const { return 0 == get_free(); }      // Test if the ringbuffer is full.
+    inline void   reset() { m_Head = m_Tail = m_Avail = 0; }       // reset(but not clear) data in buffer
+    
+    inline void   reset_to_zero()
+    {
+        // reset to zero data in buffer
+        m_Head = m_Tail = m_Avail = 0;
+        memset(m_BUF, 0, sizeof(m_BUF));
+    }
+
+    // add element to buffer
+    bool push(T& data)
+    {
+        if (is_full()) 
+        {
+            return false;
+        }
+
+        m_BUF[m_Head] = data;
+        m_Head++;
+        m_Head %= m_Size; // loopback
+        m_Avail++; 
+
+        return true;
+    }
+
+    // ejects the element from the buffer
+    T pop()
+    {
+        T result;
+
+        if (m_Avail > 0)
+        {
+            result = m_BUF[m_Tail];
+            memset(&m_BUF[m_Tail], 0, sizeof(T));
+            m_Tail++;         
+            m_Tail %= m_Size; // loopback
+            m_Avail--;           
+        }
+
+        return result;
+    }
+
+    // ejects the element from the buffer by index
+    T pop_item(size_t index)
+    {
+        T result;
+        size_t buf_idx;
+        size_t buf_idx_p1; 
+        size_t buf_idx_m1;
+
+        if(index > m_Avail)
+            return result;   // null result
+
+        if (m_Avail > 0)
+        {
+            if((index + 1) == m_Avail) // last element in buffer
+            {
+                result = m_BUF[m_Head - 1];
+
+                (m_Head) ? (m_Head--) : m_Head = (m_Size - 1); // loopback
+                m_Avail--;
+                
+                memset(&m_BUF[m_Head], 0, sizeof(T));
+                return result;
+            }
+
+            if(index == 0) // first element in buffer
+            {
+                result = m_BUF[m_Tail];
+                memset(&m_BUF[m_Tail], 0, sizeof(T));
+                m_Tail++;
+                m_Tail %= m_Size; // loopback
+                m_Avail--;
+                return result;
+            } 
+            
+            result = m_BUF[(m_Tail + index) % m_Size];
+
+            if((index + 1) > (m_Avail/2))
+            {                
+                size_t remaining_elements = m_Avail - (index + 1);
+                
+                for(size_t i = index; i < (index + remaining_elements); i++)
+                {
+                    buf_idx = (m_Tail + i) % m_Size;
+                    buf_idx_p1 = (m_Tail + (i + 1)) % m_Size;
+                    
+                    memcpy(&m_BUF[buf_idx],&m_BUF[buf_idx_p1],sizeof(T));
+                } 
+                
+                (m_Head) ? (m_Head--) : m_Head = (m_Size - 1); // loopback                
+                m_Avail--;
+                
+                memset(&m_BUF[m_Head], 0, sizeof(T));
+            }
+            else
+            { 
+                for(size_t i = index; i > 0; i--)
+                {
+                    buf_idx = (m_Tail + i) % m_Size;
+                    buf_idx_m1 = (m_Tail + (i - 1)) % m_Size;
+                    
+                    memcpy(&m_BUF[buf_idx],&m_BUF[buf_idx_m1],sizeof(T));
+                }
+                
+                memset(&m_BUF[m_Tail], 0, sizeof(T));
+                m_Tail++;
+                m_Tail %= m_Size; // loopback
+                m_Avail--;
+            }            
+        }
+
+        return result;
+    }
+
+
+    // Look first(tail) element putted in buffer
+    T* peek()  
+    {
+        if(is_empty())
+            return nullptr;
+
+        return &(m_BUF[m_Tail]);
+    }
+
+    // Look what is putted in buffer by index
+    T* peek_item(size_t index)  
+    {
+        if(is_empty() || index > m_Size)
+            return nullptr;
+
+        return &(m_BUF[(m_Tail + index) % m_Size]);
+    }
+
+    // overwrite existing element in queue
+    void write_item(size_t index, T& data) 
+    {                
+        if (data > m_Size) { return; }
+
+        m_BUF[(m_Tail + index) % m_Size] = data;
+    }
+
+private:
+    T            m_BUF[BUF_SIZE];     // ring buffer itself
+    const size_t m_Size;              // buffer size
+    size_t       m_Avail;             // available elements in buffer
+    size_t       m_Head;
+    size_t       m_Tail;
+};
+
+#endif  //_RINGBUFFER_H
